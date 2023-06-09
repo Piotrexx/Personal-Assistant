0e606a6bf9aae78a2ae4e2dfd49bf510453897ea
Peter <piotr.lopatecki@gmail.com>
2023-06-09 21:54:11 +0200

    added comments

--- main.py
+++ main.py
@@ -6,26 +6,23 @@ import datetime
 import wikipedia
 r = sr.Recognizer() # initiaze the speech recognizer
 
-def Greetings():
+def Greetings(): # function that's greet the user depending on the day time
     hour=datetime.datetime.now().hour
     if hour>=0 and hour<12:
         tts("Hello,Good Morning")
-        print("Hello,Good Morning")
     elif hour>=12 and hour<18:
         tts("Hello,Good Afternoon")
-        print("Hello,Good Afternoon")
     else:
         tts("Hello,Good Evening")
-        print("Hello,Good Evening")
 
-def listen():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        audio_data = r.record(source, duration=5) 
+def listen(): # simple function to record commands
+    r = sr.Recognizer() # initiaze the speech recognizer
+    with sr.Microphone() as source: # using microphone 
+        audio_data = r.record(source, duration=5)  # record will have duration of 5 seconds 
         print("Listening...")
 
         try:
-            statement = r.recognize_google(audio_data)
+            statement = r.recognize_google(audio_data) # trying to convert speech to text
             print(f'You said {statement}')
             return statement
         
@@ -39,15 +36,15 @@ Greetings()
 
 
 if __name__ == '__main__':
-    while True:
+    while True: # infinite loop that wait for command 
         tts("Hello, how can I help ?")
         command = listen().lower()
         if command == 0:
             continue
-        if "good bye" in command or "bye" in command or "stop" in command:
+        if "good bye" in command or "bye" in command or "stop" in command: # if command (your sentence) contains good bye, bye or stop, program will turn off
             tts("Personal AI shutting down")
             break
-        if 'wikipedia' in command:
+        if 'wikipedia' in command: # search for wikipedia
             tts('Searching Wikipedia...')
             command =command.replace("wikipedia", "")
             results = wikipedia.summary(command, sentences=3)
